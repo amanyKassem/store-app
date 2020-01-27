971f619734429ba6109eda8f79657ce6f638763a
amanyKassem <amany.kassem95@gmail.com>
2020-01-27 15:40:00 +0200

    cart

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -21,7 +21,7 @@ export default class Modal extends Component {
                                 <ModalContainer>
                                     <div className={'row'}>
                                         <div id={'modal'}
-                                             className="col-8 mx-auto col-md-6 col-lg-4 text-center text-capitalize p-5">
+                                             className="col-8 mx-auto col-md-6 col-lg-9 text-center text-capitalize p-5">
                                             <h5>item added to the cart</h5>
                                             <img src={img} className={'img-fluid'} alt="product"/>
                                             <h5>{title}</h5>
--- src/context.js
+++ src/context.js
@@ -12,6 +12,9 @@ class ProductProvider extends Component {
         cart:[],
         modalOpen:false,
         modalProduct:detailProduct,
+        cartSubtotal:0,
+        cartTax:0,
+        cartTotal:0,
     };
 
     componentDidMount() {
@@ -32,7 +35,7 @@ class ProductProvider extends Component {
     getItem = id => {
         const product = this.state.products.find(item => item.id === id);
         return product
-    }
+    };
 
     handelDetail = (id) => {
         const product = this.getItem(id);
@@ -54,7 +57,7 @@ class ProductProvider extends Component {
                 products: tempProducts , cart:[...this.state.cart , product ]
             }
         }, () => {
-            console.log(this.state)
+            this.addTotals();
         })
     };
 
@@ -71,6 +74,86 @@ class ProductProvider extends Component {
         })
     };
 
+    increment = (id) => {
+        let tempCart = [...this.state.cart];
+        const selectedProduct = tempCart.find(item => item.id === id);
+
+        const index = tempCart.indexOf(selectedProduct);
+        const product = tempCart[index];
+
+        product.count = product.count + 1;
+        product.total = product.count * product.price;
+
+        this.setState(() => {return {cart: [...tempCart]}} , () => {this.addTotals()})
+    };
+
+    decrement = (id) => {
+        let tempCart = [...this.state.cart];
+        const selectedProduct = tempCart.find(item => item.id === id);
+
+        const index = tempCart.indexOf(selectedProduct);
+        const product = tempCart[index];
+
+        product.count = product.count - 1;
+
+        if(product.count === 0) {
+            this.removeItem(id)
+        }else{
+            product.total = product.count * product.price;
+
+            this.setState(() => {return {cart: [...tempCart]}} , () => {this.addTotals()})
+        }
+    };
+
+    removeItem = (id) => {
+        let tempProducts = [...this.state.products];
+        let tempCart = [...this.state.cart];
+
+        tempCart = tempCart.filter(item => item.id !== id);
+
+        const index = tempProducts.indexOf(this.getItem(id));
+        let removeProduct = tempProducts[index];
+        removeProduct.inCart = false;
+        removeProduct.count = 0;
+        removeProduct.total = 0;
+
+        this.setState(
+            () => {
+                return {
+                    cart: [...tempCart],
+                    products: [...tempProducts]
+                };
+            },
+            () => {
+                this.addTotals()
+            }
+        )
+    };
+
+    clearCart = (id) => {
+        this.setState(() => {
+            return {cart : []};
+        } , () => {
+            this.setProducts();
+            this.addTotals();
+        })
+    };
+
+    addTotals = () => {
+        let subTotal = 0;
+        this.state.cart.map(item => (subTotal += item.total));
+        const tempTax = subTotal * 0.1;
+        const tax = parseFloat(tempTax.toFixed(2));
+        const total = subTotal + tax;
+        this.setState(() => {
+            return{
+                cartSubtotal: subTotal,
+                cartTax: tax,
+                cartTotal: total
+            }
+        })
+    };
+
     render() {
         return (
             <ProductContext.Provider value={{
@@ -79,6 +162,10 @@ class ProductProvider extends Component {
                 addToCart: this.addToCart,
                 openModal:this.openModal,
                 closeModal:this.closeModal,
+                increment:this.increment,
+                decrement:this.decrement,
+                removeItem:this.removeItem,
+                clearCart:this.clearCart,
             }}>
                 {
                     this.props.children
